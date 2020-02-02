a0991840eb10e6cbb42a143083c7da59257a27e6
Mud0Sh1t <maxence.vast@free.fr>
2020-02-02 23:40:32 +0100

    Evolution Inté homepage

---
+++ public/assets/js/main.js
@@ -0,0 +1,9 @@
+//MENU REPONSIVE
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById('menu').style.display="none";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById('menu').style.display="block";
+}
